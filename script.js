79d8fd2b8c28cd5bbd126f2a8f0a9eaf46e09576
pedropaulonevescampelo-bit <pedropaulonevescampelo@gmail.com>
2026-06-05 22:07:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
-
 const fakeDatabase = [
 
 {
+
 store:"Amazon",
-price:159.90
+
+price:159.90,
+
+image:
+"https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500",
+
+link:
+"https://amazon.com"
+
 },
 
 {
+
 store:"Mercado Livre",
-price:149.90
+
+price:149.90,
+
+image:
+"https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500",
+
+link:
+"https://mercadolivre.com.br"
+
 },
 
 {
+
 store:"Magazine Luiza",
-price:167.50
+
+price:167.50,
+
+image:
+"https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500",
+
+link:
+"https://magazineluiza.com.br"
+
 },
 
 {
+
 store:"Shopee",
-price:139.90
+
+price:139.90,
+
+image:
+"https://images.unsplash.com/photo-1546868871-7041f2a55e12?w=500",
+
+link:
+"https://shopee.com.br"
+
 },
 
 {
+
 store:"Americanas",
-price:178.00
+
+price:178.00,
+
+image:
+"https://images.unsplash.com/photo-1583394838336-acd977736f90?w=500",
+
+link:
+"https://americanas.com.br"
+
 }
 
 ];
+
 
 
 function searchProduct(){
@@ -36,7 +81,8 @@ const product =
 document
 .getElementById(
 "productInput"
-).value;
+).value.trim();
+
 
 if(!product){
 
@@ -47,6 +93,7 @@ alert(
 return;
 
 }
+
 
 const results =
 
@@ -79,15 +126,24 @@ document
 "results"
 );
 
+
 area.innerHTML="";
 
+
 results.forEach(item=>{
+
 
 area.innerHTML += `
 
 <div class="card">
 
-<div>
+<img
+class="product-image"
+src="${item.image}"
+alt="${product}"
+>
+
+<div class="info">
 
 <h3>
 
@@ -95,23 +151,42 @@ ${product}
 
 </h3>
 
-<p>
+<div class="store">
 
 ${item.store}
 
-</p>
-
 </div>
-
 
 <div class="price">
 
-R$ ${item.price}
+R$ ${item.price.toFixed(2)}
 
 </div>
 
+<a
+
+class="link-button"
+
+href="${item.link}"
+
+target="_blank"
+
+>
+
+Ir para loja
+
+</a>
 
 </div>
+
+</div>
+
+`;
+
+});
+
+
+}
 
 `;
 
